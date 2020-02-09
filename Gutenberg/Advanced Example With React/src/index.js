7ab84a3071e3f618bcca1f0a857b434ada9ff894
w3bdesign <hijack_xxx@yahoo.com>
2020-02-09 02:45:33 +0100

    .

--- Gutenberg/Advanced Example With React/src/index.js
+++
@@ -1,8 +0,0 @@
-/**
- * Import blocks as components.
- */
-import "./01-static";
-import "./02-editable";
-import "./03-media";
-import "./04-extended";
-import "./05-dynamic";
--- Gutenberg/Simple Example/README.md
+++
@@ -1,1 +0,0 @@
-# Example Gutenberg block
